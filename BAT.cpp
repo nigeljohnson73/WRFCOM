ed0aa27d4e2f05fa2e03313a698ceee335dd8e2f
Nigel Johnson <nigel@nigeljohnson.net>
2022-07-04 14:01:22 +0100

    added battery monitoring

--- BAT.cpp
+++ BAT.cpp
@@ -1,23 +1,40 @@
 #include "BAT.h"
 
-//#include <BATlib.h>
+#include "Adafruit_LC709203F.h"
 
-//BAT_PCF8523 rtc;
+Adafruit_LC709203F lc;
+
 TrBAT BAT;
 
 TrBAT::TrBAT() {};
 
 void TrBAT::begin() {
+  if (!lc.begin()) {
 #if _DEBUG_
     Serial.println("BAT disconnected");
 #endif
+    return;
+  }
+  //  lc.setThermistorB(3950);
+  //  Serial.print("Thermistor B = "); Serial.println(lc.getThermistorB());
+
+  lc.setPackSize(LC709203F_APA_1000MAH);
+  //lc.setAlarmVoltage(3.8);
+  lc.setAlarmVoltage(0);
+  lc.setAlarmRSOC(0);
+
+#if _DEBUG_
+  Serial.print("BAT initialised: 0x");
+  Serial.println(lc.getICversion(), HEX);
+#endif
+_enabled = true;
 }
 
 void TrBAT::loop() {
 }
 
 double TrBAT::getCapacityPercent() {
-	if(!isEnabled()) return 0.;
+  if (!isEnabled()) return 0.;
 
-	return 0.;
+  return lc.cellPercent();
 }
